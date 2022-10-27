63501ea6b84e5e6ce574e5845056f3886cd3a0bf
angparbal23 <>
2022-10-27 17:52:40 +0000

    Initial commit

---
+++ FlagDrawing/Flags.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Flags{
+  public  static  void  main(String[]  args){
+    JFrame theGUI = new JFrame();
+    theGUI.setSize(300, 200);
+theGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    JPanel northPanel = new JPanel();  
+    northPanel.setBackground(Color.black);
+    JPanel eastPanel = new JPanel();  
+    eastPanel.setBackground(Color.red);
+    JPanel centerPanel = new JPanel();  
+    centerPanel.setBackground(Color.yellow);
+    Container pane = theGUI.getContentPane();
+    pane.setLayout(new GridLayout(3, 1));
+    pane.add(northPanel);
+    pane.add(eastPanel);
+    pane.add(centerPanel);
+    theGUI.setVisible(true);
+  }
+}
