dacfe9296b3988807c745f8144b2029ad472378a
angparbal23 <116588827+anglepb7@users.noreply.github.com>
2022-10-27 18:05:30 +0000

    Changed GUIWindow3 class to Flags class

--- FlagDrawing/Main.java
+++ FlagDrawing/Main.java
@@ -3,8 +3,8 @@ import javax.swing.*;
 import javax.swing.border.Border;
 
 public class Main {
-
   public static void main(String[] args) {
-    GUIWindow3.main(args);
+    System.out.println("GUI3");
+    Flags.main(args);
   }
 }
